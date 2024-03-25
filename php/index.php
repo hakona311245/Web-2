5ccf5b247d9eea89cc8e81941bd08d0eb79634e5
kohi-vip <148733688+kohi-vip@users.noreply.github.com>
2024-03-25 16:29:48 +0700

    delete stuff

--- php/index.php
+++
@@ -1,9 +0,0 @@
-CREATE TABLE TaiKhoanNhanVien (
-    staff_id INT(10) PRIMARY KEY not null AUTO_INCREMENT,
-    staff_name VARCHAR(100) not null,
-    phone VARCHAR(10) not null,
-    email VARCHAR(100) not null,
-    address VARCHAR(255) not null,
-    role varchar(20) not null,
-    create_at datetime 
-);
