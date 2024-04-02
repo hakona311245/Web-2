20dbe7ae48e4534ede30d61eb20eb4621a825ffd
hakona311245 <0931771748khoa@gmail.com>
2024-04-02 21:03:41 +0700

    minor changes to the product card

--- login.php
+++ login.php
@@ -5,17 +5,23 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Responsive Login and Signup Form </title>
+        <title>SG Tech</title>
 
         <!-- CSS -->
         <link rel="stylesheet" href="css/login.css">
-                
+        <script src="https://kit.fontawesome.com/8c6957fe62.js" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/homestyle.css"/>
         <!-- Boxicons CSS -->
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
                         
     </head>
 
     <body>
+
+    <main>
+    
+
         <section class="container forms">
             <div class="form login">
                 <div class="form-content">
@@ -30,7 +36,7 @@
                         </div>
 
                         <div class="form-link">
-                            <a href="#" class="forgot-pass">Quên mật khẩu?</a>
+                            
                         </div>
 
                         <div class="field button-field">
@@ -48,8 +54,16 @@
 
             </div>
         </section>
-
+        </main>
+ 
         <!-- JavaScript -->
+        
+
+        <script src="js/headerandfooter.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min. js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="script.js"></script>
     </body>
 </html>
