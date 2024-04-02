62a87e27db57f849438415badfc5bdd13d756eab
hakona311245 <0931771748khoa@gmail.com>
2024-04-02 20:50:48 +0700

    Finishing home page & add header footer to register then login

--- Home.php
+++ Home.php
@@ -84,8 +84,14 @@
             </div>
         </div>
     </div>
-    <div class="container">
-  
+    <div>
+      <div class="black-bar">
+      <a href="your-target-page-url.html" class="product-link">LAPTOP GAMING</a>
+      </div>
+    </div>  
+    <div class="container product-showcase">
+    
+    
     <div class="product-card">
     <a href="your-target-page-url.html" class="product-link">
     <div class="product-img-container">
@@ -94,9 +100,11 @@
    
         
     </div>
-    
+    </a>
     <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
         <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
         <div class="product-specs">
             <div>CPU: i7-13620H</div>
             <div>GPU: RTX 4070</div>
@@ -105,14 +113,321 @@
             <div>Màn: 15.6" FHD</div>
             <div>RAM: 16 GB</div>
         </div>
+      
         <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
     </div>
     </div>
-</a>
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+
+
+    </div>
+    <BR>
+    <div>
+      <div class="black-bar">
+      <a href="your-target-page-url.html" class="product-link">LAPTOP VĂN PHÒNG</a>
+      </div>
+    </div>  
+    <div class="container product-showcase">
+    
+    
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+
+
     </div>
     
-
+    <div>
+      <div class="black-bar">
+      <a href="your-target-page-url.html" class="product-link">LAPTOP ĐỒ HOẠ</a>
+      </div>
+    </div>  
+    <div class="container product-showcase">
     
+    
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+    <div class="product-card">
+    <a href="your-target-page-url.html" class="product-link">
+    <div class="product-img-container">
+    
+      <img src="img/productcard/asustuff15.webp" alt="Product Image" class="product-img">
+   
+        
+    </div>
+    </a>
+    <div class="product-info">
+    <a href="your-target-page-url.html" class="product-link">
+        <div class="product-name">Laptop gaming ASUS ROG Zephyrus G14 GA403UU QS101W</div>
+        </a>
+        <div class="product-specs">
+            <div>CPU: i7-13620H</div>
+            <div>GPU: RTX 4070</div>
+            <div>RAM: 16 GB</div>
+            <div>SSD: 512 GB</div>
+            <div>Màn: 15.6" FHD</div>
+            <div>RAM: 16 GB</div>
+        </div>
+      
+        <button class="add-to-cart-btn">Thêm Vào Giỏ Hàng </button>
+    </div>
+    </div>
+
+
+
+    </div>
 
     </main>
   <footer-template></footer-template>
