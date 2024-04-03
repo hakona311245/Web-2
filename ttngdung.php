87825b771645e79bacee1f2591231ddaa797980b
thungnguyen <163085995+thungnguyen@users.noreply.github.com>
2024-04-03 21:49:09 +0700

    phần thông tin người dùng
    có thể xem và chỉnh sửa - đã chuyển file qua php

---
+++ ttngdung.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/stylettngdung.css">
+</head>
+<body>
+    <section class="signup page_customer_account">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xs-12 col-sm-12 col-lg-3 col-left-ac">
+                    <div class="block-account">
+                        <h5 class="title-account">Trang tài khoản</h5>
+                        <p>Xin chào, <span style="color:#141414;">Thành Hưng</span>&nbsp;!</p>
+                        <ul>
+                            <li>
+                                <a disabled="disabled" href="ttngdung.php" class="title-info active" title="Thông tin tài khoản" >Thông tin tài khoản</a>
+                            </li>
+                            <li>
+                                <a class="title-info" href="ttngdung1.php" title="Đơn hàng của bạn">Thay đổi thông tin</a>
+                            </li>
+                            <li>
+                                <a class="title-info" href="ttngdung2.php" title="Đổi mật khẩu">Đổi mật khẩu</a>
+                            </li>
+                            <li>
+                                <a class="title-info" href="login.php" title="Đăng xuất">Đăng xuất</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-lg-9 col-right-ac">
+                    <h1 class="title-head margin-top-0">Thông tin tài khoản</h1>
+                    <div class="form-signup name-account m992">
+                        <p><strong>Họ tên:</strong> Thành Hưng</p>
+                        <p> <strong>Email:</strong> thanhhungnguyen8204@gmail.com</p>
+
+                        <p> <strong>Điện thoại:</strong> 0376640875 </p>
+
+
+                        <p><strong>Công ty:</strong> Đại học Sài Gòn</p>
+
+
+                        <p><strong>Địa chỉ :</strong> Ấp 1B xã vĩnh lộc A huyện Bình Chánh, Huyện Bình Chánh, TP Hồ Chí Minh, Việt Nam</p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <div id="js-global-alert" class="alert alert-success" role="alert">
+        <button type="button" class="close"><span aria-hidden="true"><span aria-hidden="true">&times;</span></span></button>
+        <h5 class="alert-heading"></h5>
+        <p class="alert-content"></p>
+    </div>
+</body>
+</html>
