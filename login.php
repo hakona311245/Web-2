3f1228f6f6dec814f48d5a7621d66d8b1ec76663
kohi-vip <148733688+kohi-vip@users.noreply.github.com>
2024-04-07 16:11:45 +0700

    sửa lại login
    bỏ file css login gốc
    login.css->tạm bỏ file này.css

--- login.php
+++ login.php
@@ -1,69 +1,44 @@
-<!DOCTYPE html>
-<!-- Coding By CodingNepal - codingnepalweb.com -->
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SG Tech</title>
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="css/login.css">
-        <script src="https://kit.fontawesome.com/8c6957fe62.js" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/homestyle.css"/>
-        <!-- Boxicons CSS -->
-        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-                        
-    </head>
-
-    <body>
-
-    <main>
-    
-
-        <section class="container forms">
-            <div class="form login">
-                <div class="form-content">
-                    <header>ĐĂNG NHẬP</header>
-                    <form action="includes/formhandler.inc.php" method="post">
-                        <div class="field input-field">
-                            <input type="text" placeholder="Tên đăng nhập" class="input">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Password" class="password">
-                        </div>
-
-                        <div class="form-link">
-                            
-                        </div>
-
-                        <div class="field button-field">
-                            <button>Đăng nhập</button>
-                        </div>
-                    </form>
-
-                    <div class="form-link">
-                        <span>Bạn chưa có tài khoản? <a href="register.php"class="link signup-link">Đăng ký tại đây</a></span>
-                    </div>
-                </div>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SG Tech</title>
+    <script src="https://kit.fontawesome.com/8c6957fe62.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/register.css">
+    <link rel="stylesheet" href="css/header&footer.css"/>
+  </head>
 
 
-               
+<body>
+  <header-template></header-template>
+  <main>
+  <div class="wrapper">
+    <h2>ĐĂNG NHẬP</h2>
+    <form action="includes/formhandler.inc.php" method="post">
+      <div class="input-box">
+        <input type="text" name="user_name" placeholder="Tên đăng nhập" required>
+      </div>
+      <div class="input-box">
+        <input type="password" name="user_pwd" placeholder="Password" required>
+      </div>
 
-            </div>
-        </section>
-        </main>
- 
-        <!-- JavaScript -->
-        
+      <div class="input-box button">
+        <input type="Submit" value="Đăng nhập">
+      </div>
+      <div class="text">
+        <h3>Bạn chưa có tài khoản? <a href="register.php">Đăng kí</a></h3>
+      </div>
+    </form>
+  </div>
+  </main>
 
-        <script src="js/headerandfooter.js"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min. js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <script src="script.js"></script>
-    </body>
+  <footer-template></footer-template>
+
+  <script src="js/headerandfooter.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
