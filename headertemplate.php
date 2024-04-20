2f27b8438325d6b6173896535dbb544b12a19cc2
thungnguyen <163085995+thungnguyen@users.noreply.github.com>
2024-04-21 01:40:58 +0700

    header test
    123

--- headertemplate.php
+++ headertemplate.php
@@ -1,3 +1,21 @@
+<?php
+
+echo'để dành đừng xóa để hiện tên khách hàng len header';
+
+// require_once("./testadmin/databaseadmin.php");
+// require_once("./testadmin/session.php");
+// require_once("./testadmin/function.php");
+
+// session_start();
+// $user_name = $_SESSION['user_username'];
+// $userInfo = getRaw("SELECT * FROM taikhoannguoidung WHERE user_name ='$user_name'");
+
+// echo '<pre>';
+// print_r($userInfo);
+// echo '</pre>';
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,14 +26,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/homestyle.css"/>
-
 </head>
 <body style="height:2000px">
     
 <header>
     <div class="header">
         <div class="logo-container">
-
             <div class="logo-row">
                 <a href="Home.php"><img width="100" height="123" src="img/logopng.png"></a>
             </div>
